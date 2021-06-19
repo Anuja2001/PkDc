e11145f16da90c232824e86c1c3cfc5ee67e580d
Swarangi <swarangi.patil@gmail.com>
2021-06-19 22:33:16 +0530

    splash screen

---
+++ app/src/main/java/com/example/pokedoc/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.pokedoc;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,RoleActivity.class));
+                finish();
+            }
+        },1500);
+    }
+}
